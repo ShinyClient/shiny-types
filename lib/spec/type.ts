3d3b3f5cce08ea3f519cafd0583658beffb949ab
Biraj Kirat Dewas Rai <70630924+birajrai@users.noreply.github.com>
2023-12-02 14:36:28 +0545

    Update type.ts  |  Added Fabric Support

--- lib/spec/type.ts
+++ lib/spec/type.ts
@@ -1,58 +1,67 @@
 export enum Type {
-
-    Library = 'Library',
-    ForgeHosted = 'ForgeHosted',
-    Forge = 'Forge',
-    LiteLoader = 'LiteLoader',
-    ForgeMod = 'ForgeMod',
-    LiteMod = 'LiteMod',
-    File = 'File',
-    VersionManifest = 'VersionManifest'
-
+  Library = "Library",
+  FabricHosted = "FabricHosted",
+  Fabric = "Fabric",
+  FabricMod = "FabricMod",
+  ForgeHosted = "ForgeHosted",
+  Forge = "Forge",
+  ForgeMod = "ForgeMod",
+  LiteLoader = "LiteLoader",
+  LiteMod = "LiteMod",
+  File = "File",
+  VersionManifest = "VersionManifest",
 }
 
 export interface TypeMetadata {
-
-    id: string
-    defaultExtension?: string
-
+  id: string;
+  defaultExtension?: string;
 }
 
-export const TypeMetadata: {[property: string]: TypeMetadata} = {
-
-    Library: {
-        id: Type.Library,
-        defaultExtension: 'jar'
-    },
-    /**
-     * @deprecated Will be replaced by Types.Forge.
-     */
-    ForgeHosted: {
-        id: Type.ForgeHosted,
-        defaultExtension: 'jar'
-    },
-    Forge: {
-        id: Type.Forge,
-        defaultExtension: 'jar'
-    },
-    LiteLoader: {
-        id: Type.LiteLoader,
-        defaultExtension: 'jar'
-    },
-    ForgeMod: {
-        id: Type.ForgeMod,
-        defaultExtension: 'jar'
-    },
-    LiteMod: {
-        id: Type.LiteMod,
-        defaultExtension: 'litemod'
-    },
-    File: {
-        id: Type.File
-    },
-    VersionManifest: {
-        id: Type.VersionManifest,
-        defaultExtension: 'json'
-    }
-
-}
+export const TypeMetadata: { [property: string]: TypeMetadata } = {
+  Library: {
+    id: Type.Library,
+    defaultExtension: "jar",
+  },
+  /**
+   * @deprecated Will be replaced by Types.Forge.
+   */
+  FabricHosted: {
+    id: Type.FabricHosted,
+    defaultExtension: "jar",
+  },
+  Fabric: {
+    id: Type.Fabric,
+    defaultExtension: "jar",
+  },
+  FabricMod: {
+    id: Type.FabricMod,
+    defaultExtension: "jar",
+  },
+  ForgeHosted: {
+    id: Type.ForgeHosted,
+    defaultExtension: "jar",
+  },
+  Forge: {
+    id: Type.Forge,
+    defaultExtension: "jar",
+  },
+  ForgeMod: {
+    id: Type.ForgeMod,
+    defaultExtension: "jar",
+  },
+  LiteLoader: {
+    id: Type.LiteLoader,
+    defaultExtension: "jar",
+  },
+  LiteMod: {
+    id: Type.LiteMod,
+    defaultExtension: "litemod",
+  },
+  File: {
+    id: Type.File,
+  },
+  VersionManifest: {
+    id: Type.VersionManifest,
+    defaultExtension: "json",
+  },
+};
